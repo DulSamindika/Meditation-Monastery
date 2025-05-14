7eec26c9c216dd97d7c06d0bc8593c7ee8a89c77
csheha <chanudishehani22@gmail.com>
2025-05-14 19:26:40 +0530

    updated App.js file

--- medi/src/App.js
+++ medi/src/App.js
@@ -1,11 +1,21 @@
 //import logo from './logo.svg';
-import './App.css';
+import MeditationYogaHome from "../src/pages/MeditationYoga/MeditationYogaHome"; // Import the MeditationYogaHome component
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; // Import needed for routing
 
 function App() {
+  // route to the Meditation Yoga Home page
+  const router = createBrowserRouter([
+    {
+      path: "/meditation-yoga",
+      element: <MeditationYogaHome />,
+    },
+  ]);
+
   return (
     <div className="App">
       <header className="App-header">
-       
+        <RouterProvider router={router} />
       </header>
     </div>
   );
