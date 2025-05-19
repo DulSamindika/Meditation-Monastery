7b87a5a73bb69003590262954145b2913893c405
Dulshani <waddsamindika@gmail.com>
2025-05-19 08:01:53 -0700

    home-two

--- medi/src/App.js
+++ medi/src/App.js
@@ -2,8 +2,18 @@
 import './App.css';
 import Home from './components/Home';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // animation duration in ms
+      once: true      // whether animation should happen only once
+    });
+  }, []);
+
   return (
     <>
     <Router>
