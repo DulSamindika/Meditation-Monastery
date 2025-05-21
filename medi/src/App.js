4a82dfd4acddccafda14aa68799900274d93ad72
Darshani24 <radkumari@std.appsc.sab.ac.lk>
2025-05-21 12:53:46 +0530

    Import the home page

--- medi/src/App.js
+++ medi/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Accommodation from './pages/Accommodation/Accommodation';
-// import MeditationYogaHome from './pages/MeditationYoga/MeditationYogaHome'; // Uncomment if it exists
+import Home from './components/Home.jsx'
+ import MeditationYogaHome from './pages/MeditationYoga/MeditationYogaHome'; // Uncomment if it exists
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/accommodation/*" element={<Accommodation />} />
-        {/* <Route path="/meditation-yoga" element={<MeditationYogaHome />} /> */}
+        <Route path="/meditation-yoga" element={<MeditationYogaHome />} /> 
       </Routes>
     </Router>
   );
