614b6a885ab9c931f7de710aad97ce473b625d92
csheha <chanudishehani22@gmail.com>
2025-05-21 23:11:33 +0530

    Events section added

---
+++ medi/src/components/MeditationYoga/EventsSection.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function EventsSection() {
+  return (
+    <>
+      <div className="events-section">
+        <div className="title">
+          <div className="section-title">
+            <div className="tagline-wrapper">
+              <span className="text">Explore</span>
+            </div>
+            <div className="content">
+              <span className="heading">Upcoming Events</span>
+              <span className="text">
+                Discover tranquility with our upcoming meditation events –
+                guided sessions, workshops, and retreats designed to bring peace
+                to your journey
+              </span>
+            </div>
+          </div>
+          <div className="action"></div>
+        </div>
+        <div className="content"></div>
+      </div>
+    </>
+  );
+}
