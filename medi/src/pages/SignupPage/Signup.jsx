5b8780fea75adac3703cda4f0ebdef2fc6a84c97
Darshani24 <radkumari@std.appsc.sab.ac.lk>
2025-06-11 12:47:58 +0530

    Signup textbox added

--- medi/src/pages/SignupPage/Signup.jsx
+++ medi/src/pages/SignupPage/Signup.jsx
@@ -9,9 +9,13 @@ const Signup = () => {
       <div className='text-xl text-center italic font-light md:mt-2 mb-3'>KithalElla</div>
     </div>
        
+       <div  className='flex items-center justify-center'>
+       <div className='bg-gray-300 bg-opacity-80 w-[316px] lg:w-[330px] h-[415px] lg:h-[450px] rounded-[8px] p-[13px] md:ml-[75px] '>
+
             <h1 className='text-center p-[12px] font-bold text-[20px] lg:text-[30px]'>Sign Up</h1>
 
-
+       </div>
+        </div> 
       </div>
   )
 }
