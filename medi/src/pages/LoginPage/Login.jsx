e4e93d5846b4ef1c57cbe373dbb35d36f4234a00
Darshani24 <radkumari@std.appsc.sab.ac.lk>
2025-06-11 12:01:15 +0530

    Landing Text Added

--- medi/src/pages/LoginPage/Login.jsx
+++ medi/src/pages/LoginPage/Login.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='min-h-screen bg-cover bg-center flex flex-col md:flex-row  justify-center'style={{ backgroundImage: "url('assets/img/BgLog.png')",}}>
+      
+      <div className='text-2xl text-white md:text-[23px] lg:text-3xl md:mt-[80px] text-center md:ml-[-150px] font-serif leading-snug'>
+      <div className='md:ml-[35px]'>Meditation Monastery</div>
+      <div className='text-xl text-center italic font-light md:mt-2 mb-3'>KithalElla</div>
+    </div>
+
        </div>
   )
 }
