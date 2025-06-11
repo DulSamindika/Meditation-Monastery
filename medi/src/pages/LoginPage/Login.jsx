9f1349bc25aed77fd217fd1d7c7e6c9b2871a226
Darshani24 <radkumari@std.appsc.sab.ac.lk>
2025-06-11 11:03:51 +0530

    save changes

--- medi/src/pages/LoginPage/Login.jsx
+++ medi/src/pages/LoginPage/Login.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='min-h-screen bg-cover bg-center flex flex-col md:flex-row  justify-center'style={{ backgroundImage: "url('assets/img/BgLog.png')",}}>
-      
-    </div>
+    <div>Login</div>
   )
 }
 
