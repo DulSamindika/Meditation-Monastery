ffba531df64f7c04b15f3edab643e7af76bde5dd
Darshani24 <radkumari@std.appsc.sab.ac.lk>
2025-06-11 12:45:25 +0530

    Signuppage landing text added

--- medi/src/pages/SignupPage/Signup.jsx
+++ medi/src/pages/SignupPage/Signup.jsx
@@ -4,6 +4,10 @@ const Signup = () => {
   return (
       <div className='min-h-screen bg-cover bg-center flex flex-col md:flex-row  justify-center  gap-x-0 md:gap-x-1 lg:gap-x-[100px]'style={{ backgroundImage: "url('assets/img/BgReg.jpeg')",}}>
 
+        <div className='text-2xl text-white md:text-[23px] lg:text-3xl md:mt-[80px] text-center md:ml-[-150px] font-serif leading-snug'>
+      <div className='md:ml-[35px]'>Meditation Monastery</div>
+      <div className='text-xl text-center italic font-light md:mt-2 mb-3'>KithalElla</div>
+    </div>
        
             <h1 className='text-center p-[12px] font-bold text-[20px] lg:text-[30px]'>Sign Up</h1>
 
