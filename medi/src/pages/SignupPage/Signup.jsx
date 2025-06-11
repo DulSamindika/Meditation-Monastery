882fdc68a09b02d58e9e43d5b11f86c6c3670a98
Darshani24 <radkumari@std.appsc.sab.ac.lk>
2025-06-11 12:41:28 +0530

    Signup text added

--- medi/src/pages/SignupPage/Signup.jsx
+++ medi/src/pages/SignupPage/Signup.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Signup = () => {
   return (
       <div className='min-h-screen bg-cover bg-center flex flex-col md:flex-row  justify-center  gap-x-0 md:gap-x-1 lg:gap-x-[100px]'style={{ backgroundImage: "url('assets/img/BgReg.jpeg')",}}>
-        
+
+       
+            <h1 className='text-center p-[12px] font-bold text-[20px] lg:text-[30px]'>Sign Up</h1>
+
+
       </div>
   )
 }
