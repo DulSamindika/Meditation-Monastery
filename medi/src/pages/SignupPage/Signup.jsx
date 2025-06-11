56f8c4c69ac2189356b99f761e7b788c16372cec
Darshani24 <radkumari@std.appsc.sab.ac.lk>
2025-06-11 13:02:58 +0530

    Signup button color changed

--- medi/src/pages/SignupPage/Signup.jsx
+++ medi/src/pages/SignupPage/Signup.jsx
@@ -40,7 +40,7 @@ const Signup = () => {
                  <button className='hover:bg-[#1acc8d] bg-[#013220] px-3 py-2 rounded-[14px] text-[16px] lg:text-[18px] text-white w-[130px] lg:w-[180px] '>Sign Up</button>
           </div>
 
-                <label className='text-[16px] pt-3 text-gray-800 '> Not Registerd yet? <button className='text-gray-900 font-semibold' >Login </button></label>
+                <label className='text-[16px] pt-3 text-gray-800 '> Not Registerd yet? <button className='text-gray-900 font-semibold bg-transparent p-0 m-0 border-none' >Login </button></label>
 
             </div>
        </div>
