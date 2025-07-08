8861ba4b43329c11c983680263cddb5ca5ed83cc
Darshani <radkumari@std.appsc.sab.ac.lk>
2025-07-08 21:29:57 +0530

    UI bug fixed

--- medi/src/pages/LoginPage/Login.jsx
+++ medi/src/pages/LoginPage/Login.jsx
@@ -28,10 +28,10 @@ const Login = () => {
   };
 
   return (
-    <div className='min-h-screen bg-cover bg-center ' style={{ backgroundImage: "url('assets/img/BgLog.png')" }}>
-      <div className='md:flex md:justify-center md:pt-[150px]'>
+    <div className='min-h-screen bg-cover -mt-[120px] bg-center ' style={{ backgroundImage: "url('assets/img/BgLog.png')" }}>
+      <div className='md:flex md:justify-center md:pt-[150px] pt-[60px]'>
 <div className='text-2xl text-white md:text-[23px] lg:text-4xl  font-bold  font-serif leading-snug'>
-        <div className='md:ml-[35px]'>Meditation Monastery</div>
+        <div className='md:ml-[35px] text-center'>Meditation Monastery</div>
         <div className='text-xl text-center italic font-light md:mt-2 mb-3'>KithalElla</div>
       </div>
 
